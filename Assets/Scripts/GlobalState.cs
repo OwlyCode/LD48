5da1e0f2104ef3039cb07b9e7746dca890b6035a
Vincent ABEL-GRIMALT <vincent.abel.grimalt@gmail.com>
2021-04-24 15:57:11 +0200

    Engine for Generating level from level File
    Add Placeholder, Prefabs Required, Attempt on Multiscene loading
    Add GlobalState handler, and multiple levels.

--- Assets/Scripts/GlobalState.cs
+++ Assets/Scripts/GlobalState.cs
@@ -1,24 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GlobalState : MonoBehaviour
 {
-    public GameObject TWall;
-    public GameObject TVoid;
-    public GameObject TInDoor;
-    public GameObject TOutDoor;
-    public GameObject THole;
+    public GameObject Wall;
+    public GameObject Floor;
+    public GameObject InDoor;
+    public GameObject OutDoor;
+    public GameObject Hole;
+    public string level;
+
+    void SourceLevel(string FileName)
+    {
+        string[] lines = File.ReadAllLines("Assets/Levels/" + FileName);
+        int y = 0;
+        int x = 0;
+
+        
+        foreach (string line in lines)
+        {
+            x = 0;
+            foreach (char c in line)
+            {
+                Debug.Log(c);
+                switch (c)
+                {
+                    case '.':
+                        Instantiate(Floor, new Vector3(x, y, 0), Quaternion.identity);
+                        break;
+                    case 'X':
+                        Instantiate(Wall, new Vector3(x, y, 0), Quaternion.identity);
+                        break;
+                    case 'E':
+                        Instantiate(InDoor, new Vector3(x, y, 0), Quaternion.identity);
+                        break;
+                    case 'S':
+                        Instantiate(OutDoor, new Vector3(x, y, 0), Quaternion.identity);
+                        break;
+                    case 'T':
+                        Instantiate(Hole, new Vector3(x, y, 0), Quaternion.identity);
+                        break;
+                    default:
+                        Debug.Log("Default !");
+                        break;
+                }
+                x++;
+            }
+            y++;
+        }
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SourceLevel(level);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
