27d8cf6ecb8e2434727b2069c069c8aff21cd0f6
Tristan Maindron <tmaindron@gmail.com>
2021-04-25 10:23:35 +0200

    Added death animation

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -13,9 +13,12 @@ public class Trap : MonoBehaviour
 
         GetComponentInChildren<SpriteRenderer>().enabled = true;
 
+        hero.GetComponent<Animator>().SetTrigger("Fall");
+
         manager.FadeOut(() => {
             state.RestartLevel();
             hero.transform.position = state.GetStartPosition();
+            hero.GetComponent<Animator>().SetTrigger("Respawn");
             manager.FadeIn(() => {
                 hero.GetComponent<Hero>().Respawn();
             });
